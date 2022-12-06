4484a84ed619d77f0771698a3ce7631511d17538
Giovanni Bedoya <giovedoya@gmail.com>
2022-12-06 20:41:09 +0100

    'Bug'

--- js/game.js
+++ js/game.js
@@ -32,19 +32,20 @@ _drawEnemy(){
   checkCollison(){
     this.enemies.forEach(enemy => {
     if((this.player.x >= enemy.x  && this.player.x <= enemy.x + enemy.width)
-    && (this.player.y >= enemy.y && this.player.y <= enemy.y + enemy.height)){     
+    && (this.player.y >= enemy.y && this.player.y <= enemy.y + enemy.height)){    
       this.points++;
+     // this.enemies.splice(this.enemies.indexOf(enemy),1);
     }
   })  
 } 
 _assignClickMouse(){
-    canvas.addEventListener('click', (e)=> { 
+    canvas.addEventListener('click', (e)=> {
       const rect = canvas.getBoundingClientRect();
       const x = e.clientX - rect.left;
       const y = e.clientY - rect.top; 
       this.player.x = x;
       this.player.y = y; 
-      this.checkCollison()      
+      this.checkCollison();     
   })
 
 }
@@ -77,14 +78,13 @@ _assignClickMouse(){
  _win(){
   if(this.points >= 20){
     clearInterval(this.youWin)
-    clearInterval(this.generateInterval)
+    clearInterval(this.generatorTimer)
     const winPage = document.getElementById('win-page')
     winPage.style = 'display: flex';
     const canvas = document.getElementById('canvas')
     canvas.style = 'display: none';   
   }
  }
-
 
   _clean() {
     this.ctx.clearRect(0, 0, 1000, 600);
